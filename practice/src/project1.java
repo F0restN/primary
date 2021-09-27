bf0843e01cc91394fbe5d0afc060ffbe2b500bb1
Drake Zhou <drakezhou@DrakedeMacBook-Pro.local>
2021-09-27 16:55:03 -0400

    proj #1 finalize draft

--- practice/src/project1.java
+++ practice/src/project1.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
 public class project1 {
 
     static void ques1 (int[]a, int m, int n){
-        System.out.println("HelloWorld");
+        System.out.println();
         if (a[m] <= a[m+1]){
-            System.out.println("This array is sorted in ascendant order");
+            System.out.print("This array is already sorted in ascendant order");
             System.out.println(Arrays.toString(a));
             return;
         }
@@ -59,7 +59,7 @@ public class project1 {
             if (minCursor <= aux.length-1){
                 System.arraycopy(aux,minCursor,a,mainCursor,minCursor-aux.length+1);
             }
-            System.out.println(Arrays.toString(a));
+//            System.out.println(Arrays.toString(a));
         };
     }
 
@@ -96,40 +96,117 @@ public class project1 {
         int sig1 = a.signum();
         int sig2 = b.signum();
         if (sig1 + sig2 == 0){
-            sum = sum.multiply(new BigInteger("-1"));
+            sum = sum.negate();
         }
 
         System.out.println(sum);
         return;
     }
 
+    static int[] transferNumberIntoDight(String a){
+        if (!a.contains("-")){
+            a = "+" + a;
+        }
+        String str = String.valueOf(a);
+        int[] digit;
+        if (str.contains("-")){
+            digit = new int[str.length()];
+            digit[0] = -1;
+        }else {
+            digit = new int[str.length()];
+            digit[0] = 1;
+        }
+        for(int i=1;i<str.length();i++){
+            digit[i]=Integer.parseInt(String.valueOf(str.charAt(i)));
+        }
+        return digit;
+    }
+
     static void rectangularMulti(String a, String b){
+        // Convert to dights
+        int[] digitA = transferNumberIntoDight(a);
+        int[] digitB = transferNumberIntoDight(b);
+        int lenA = digitA.length-1;
+        int lenB = digitB.length-1;
+        int finalDigits = lenA + lenB -1;
 
+        // Multiply in rectangular and pad blanks with 0
+        int[][] padMatrix = new int[lenB][finalDigits];
+        for (int i =0;i<lenB;i++){
+            for (int j = i;j<finalDigits;j++){
+                try {
+                    padMatrix[i][j] = digitA[j-i+1] * digitB[i+1];
+                } catch (ArrayIndexOutOfBoundsException error){
+                    padMatrix[i][j] = 0;
+                }
+            }
+        }
 
+        // @Test： Output Matrix
+//        for (int[] matrix : padMatrix) {
+//            System.out.println(Arrays.toString(matrix));
+//        }
+
+        // Accumulate to get each digit
+        int sum = 0;
+        int carryFlag = 0;
+        int[] digitResult = new int[finalDigits];
+        for (int i = finalDigits-1;i>=1;i--){
+            sum = carryFlag;
+            for(int j = 0;j<lenB;j++){
+                sum = sum + padMatrix[j][i];
+            }
+            digitResult[i] = sum % 10;
+            carryFlag = sum / 10;
+        }
+        sum = padMatrix[0][0] + carryFlag;
+        digitResult[0] = sum;
+
+        // Convert to number
+        BigInteger result = new BigInteger("0");
+        for(int i=0;i<digitResult.length;i++){
+            result=result.multiply(new BigInteger("10")).add(new BigInteger(String.valueOf(digitResult[i])));
+        }
+
+        // Include positive and negative sign
+        if (digitA[0] * digitB[0] < 0){
+            result = result.negate();
+        }
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
         // Array A = [0...m][m+1...n]
         // m & n are index of A
-//        int[] a;
-//        a = new int[]{3,7,9};
-//        ques1(a,0,2);
-//
-//        a= new int[]{2,7,9,1};
-//        ques1(a,2,3);
-//
-//        a = new int[]{1,7,10,15,3,8,12,18};
-//        ques1(a,3,7);
-//
-//        a = new int[]{1, 3, 5, 5, 15, 18, 21, 5, 5, 6, 8, 10, 12, 16, 17, 17, 20, 25, 28};
-//        ques1(a,6, 18);
+        int[] a;
+        a = new int[]{3,7,9};
+        ques1(a,0,2);
 
-          alaCart(new BigInteger("7000"),new BigInteger("7294"));
-          alaCart(new BigInteger("25"), new BigInteger("5038385"));
-          alaCart(new BigInteger("-59724"), new BigInteger("783"));
-          alaCart(new BigInteger("8516"), new BigInteger("-82147953548159344"));
-          alaCart(new BigInteger("45952456856498465985"), new BigInteger("98654651986546519856"));
-          alaCart(new BigInteger("-45952456856498465985"), new BigInteger("-98654651986546519856"));
+        a= new int[]{2,7,9,1};
+        ques1(a,2,3);
 
+        a = new int[]{1,7,10,15,3,8,12,18};
+        ques1(a,3,7);
+
+        a = new int[]{1, 3, 5, 5, 15, 18, 21, 5, 5, 6, 8, 10, 12, 16, 17, 17, 20, 25, 28};
+        ques1(a,6, 18);
+
+        System.out.println("=========== Ala Cart Multiplication ===========");
+//
+        alaCart(new BigInteger("7000"),new BigInteger("7294"));
+        alaCart(new BigInteger("25"), new BigInteger("5038385"));
+        alaCart(new BigInteger("-59724"), new BigInteger("783"));
+        alaCart(new BigInteger("8516"), new BigInteger("-82147953548159344"));
+        alaCart(new BigInteger("45952456856498465985"), new BigInteger("98654651986546519856"));
+        alaCart(new BigInteger("-45952456856498465985"), new BigInteger("-98654651986546519856"));
+
+        System.out.println("=========== Rectangular Multiplication ===========");
+
+        rectangularMulti("7000","7294");
+        rectangularMulti("25","5038385");
+        rectangularMulti("-59724","783");
+        rectangularMulti("8516","-82147953548159344");
+        rectangularMulti("45952456856498465985","98654651986546519856");
+        rectangularMulti("-45952456856498465985", "-98654651986546519856");
     }
 }
