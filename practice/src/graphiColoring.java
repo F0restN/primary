78e3bb73d1b01f06c97d8b2670886b997ddca622
Drake Zhou <drakezhou@DrakedeMacBook-Pro.local>
2021-10-27 14:37:25 -0400

    Done: quiz8

---
+++ practice/src/graphiColoring.java
@@ -0,0 +1,5 @@
+public class graphiColoring {
+
+    public static void main(String[] args) {
+    }
+}
--- practice/src/quiz8.java
+++ practice/src/quiz8.java
@@ -4,35 +4,67 @@ import java.util.Arrays;
 
 public class quiz8 {
 
-    static boolean isValidate(int row, int[] queen){
-        for (int i = 0;i<row;i++){
-            if (queen[row] == queen[i] || Math.abs(row-i) == Math.abs(queen[row]-queen[i])){
+    static boolean isValidate(int row, int[] queen) {
+        for (int i = 0; i < row; i++) {
+            if (queen[row] == queen[i] || Math.abs(row - i) == Math.abs(queen[row] - queen[i])) {
                 return false;
             }
         }
         return true;
-    };
+    }
 
-    static void setNextQueen(int row, int[] queen){
-        if (row >= queen.length){
-            System.out.println("complete");
+
+    /* // Another structure
+    static int setNextQueen(int count, int row, int[] queen){
+        if (row < queen.length){
+            for (int j = 0; j< queen.length;j++){
+                queen[row] = j;
+                if (isValidate(row, queen)){
+                    count = setNextQueen(count,row+1, queen);
+                }
+            }
+        } else {
+            count++;
             System.out.println(Arrays.toString(queen));
-            return;
+        }
+        return count;
+    }*/
+
+    static int setNextQueen(int count, int row, int[] queen) {
+        if (row >= queen.length) {
+            count++;
+//            System.out.println(Arrays.toString(queen));
+            return count;
         }
 
-        for (int j = 0; j< queen.length;j++){
+        for (int j = 0; j < queen.length; j++) {
             queen[row] = j;
-            if (isValidate(row, queen)){
-                setNextQueen(row+1, queen);
+            if (isValidate(row, queen)) {
+                count = setNextQueen(count, row + 1, queen);
             }
         }
+
+        return count;
     }
 
     public static void main(String[] args) {
-        int[] queen = new int[4];
-        setNextQueen(0, queen);
-//        System.out.println(Arrays.toString(result));
+        int count = 0;
 
+        int[] queen = new int[4];
+        System.out.println("When N = 4");
+        System.out.println(setNextQueen(count, 0, queen));
+
+        queen = new int[8];
+        System.out.println("When N = 8");
+        System.out.println(setNextQueen(count, 0, queen));
+
+        queen = new int[10];
+        System.out.println("When N = 10");
+        System.out.println(setNextQueen(count, 0, queen));
+
+        queen = new int[12];
+        System.out.println("When N = 12");
+        System.out.println(setNextQueen(count, 0, queen));
     }
 
 }
