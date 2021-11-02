7f8d5e16bccb0222508dc785ac2d60699604b84b
Drake Zhou <drakezhou@DrakedeMacBook-Pro.local>
2021-11-02 17:49:41 -0400

    Done: quiz9

--- practice/src/quiz9.java
+++ practice/src/quiz9.java
@@ -3,32 +3,35 @@ import java.util.ArrayList;
 
 public class quiz9 {
 
-    public ArrayList<ArrayList> ans = new ArrayList();
-
-    public void subset(int[] set, int count, ArrayList sub) {
+    static int subset(int totalNub, int[] set, int count, ArrayList sub) {
         int len = set.length;
+        // Cutting branch criterion
         if (count > len) {
-            return;
+            return totalNub;
         }
 
+        // Backtracking
         for (int i = count; i < len; i++) {
             sub.add(set[i]);
-            subset(set, i + 1, sub);
+            // Left node (means choose)
+            totalNub = subset(totalNub, set, i + 1, sub);
+            // Backtracking to last node and apply to right node(means do not choose)
             sub.remove(sub.size()-1);
         }
+
+        totalNub++;
         System.out.println(sub);
-        ans.add(new ArrayList(sub));
+        return totalNub;
     }
 
     public static void main(String[] args) {
-        quiz9 qz = new quiz9();
-
         int[] subset = new int[]{1, 2, 3};
-        ArrayList sub = new ArrayList();
-        qz.subset(subset, 0, sub);
-        System.out.println(qz.ans);
-        for (ArrayList list: qz.ans) {
-            System.out.println(list);
-        }
+        System.out.println("Total Number is: "+ subset(0, subset, 0, new ArrayList()));
+
+        subset = new int[]{1,2,3,4,5};
+        System.out.println("Total Number is: "+ subset(0, subset, 0, new ArrayList()));
+
+        subset = new int[]{1,2,3,4,5,6,7};
+        System.out.println("Total Number is: "+ subset(0, subset, 0, new ArrayList()));
     }
 }
